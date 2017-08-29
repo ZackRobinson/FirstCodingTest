7bda07f9bf9dedf5e68acf5478c2c3052a08d1e3
Zack Robinson <zwrprofessional@gmail.com>
2017-08-29 07:30:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/zack/firstcodingtest/CodingTest.java
@@ -0,0 +1,40 @@
+package com.example.zack.firstcodingtest;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Zack on 8/29/2017.
+ */
+
+public class CodingTest {
+    public static void main(String[] args) {
+
+        //problem 1
+        int num = 2;
+        if (num % 3 == 0){
+            System.out.println("fizz");
+        } else if (num % 5 == 0) {
+            System.out.println("buzz");
+        } else if ((num%3==0) && (num%5==0)) {
+            System.out.println("fizz buzz");
+        }
+    }
+
+        //problem 2
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("A");
+        stringArrayList.add("B");
+        stringArrayList.add("C");
+        stringArrayList.add("D");
+        stringArrayList.add("D");
+        stringArrayList.add("E");
+        for(int i = 0; i < stringArrayList.size(); i++) {
+            String s = stringArrayList.get(i);
+            for (int x = 0; i < stringArrayList.size(); i++)
+                if (s == stringArrayList.get(i)){
+                System.out.println(x);
+            }
+        }
+
+
+}
